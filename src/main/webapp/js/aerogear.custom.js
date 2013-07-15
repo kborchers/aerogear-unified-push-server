a6b4203957c66a4230c36d7aa0d3fc50edfd01e7
Kris Borchers <kris.borchers@gmail.com>
2013-07-15 08:51:10 -0500

    Update to latest aerogear.js

--- src/main/webapp/js/aerogear.custom.js
+++ src/main/webapp/js/aerogear.custom.js
@@ -148,98 +148,6 @@ AeroGear.isArray = function( obj ) {
     @param {Object} data - The updated data object after the new saved data has been added
  */
 
-(function( AeroGear, $, undefined ) {
-    var simpleNotifier, nativePush;
-    // Use browser push implementation when available
-    // TODO: Test for browser-prefixed implementations
-    // TODO: Actually use the native version when present
-    if ( navigator.push ) {
-        nativePush = navigator.push;
-    }
-
-    // SimplePush Default Config
-    AeroGear.SimplePush = window.AeroGearSimplePush;
-    AeroGear.SimplePush.simplePushServerURL = window.AeroGearSimplePush.simplePushServerURL || "http://" + window.location.hostname + ":7777/simplepush";
-
-    // Add push to the navigator object
-    navigator.push = (function() {
-        return {
-            register: nativePush ? nativePush.register : function() {
-                var request = {
-                    onsuccess: function( event ) {}
-                };
-
-                if ( !simpleNotifier ) {
-                    throw "SimplePushConnectionError";
-                }
-
-                simpleNotifier.subscribe({
-                    requestObject: request,
-                    callback: function( message ) {
-                        $( navigator.push ).trigger({
-                            type: "push",
-                            message: message
-                        });
-                    }
-                });
-
-                return request;
-            },
-
-            unregister: nativePush ? nativePush.unregister : function( endpoint ) {
-                simpleNotifier.unsubscribe( endpoint );
-            }
-        };
-    })();
-
-    navigator.setMessageHandler = function( messageType, callback ) {
-        var handler;
-        // TODO: Check for other browser implementations
-        if ( navigator.mozSetMessageHandler ) {
-            navigator.mozSetMessageHandler.apply( arguments );
-            return;
-        }
-
-        handler = function( event ) {
-            var message = event.message;
-            callback.call( this, message );
-        };
-
-        $( navigator.push ).on( messageType, handler );
-    };
-
-    // Create a Notifier connection to the Push Network
-    simpleNotifier = AeroGear.Notifier({
-        name: "agPushNetwork",
-        type: "SimplePush",
-        settings: {
-            connectURL: AeroGear.SimplePush.simplePushServerURL
-        }
-    }).clients.agPushNetwork;
-
-    simpleNotifier.connect({
-        onConnect: function( data ) {
-            var pushStore = JSON.parse( localStorage.getItem("ag-push-store") || '{}' ),
-                channels = pushStore.channels || [];
-
-            // Subscribe to any new channels
-            for ( var channel in channels ) {
-                if ( channels[ channel ].state === "new" ) {
-                    simpleNotifier.subscribe({
-                        channelID: channels[ channel ].channelID,
-                        callback: function( message ) {
-                            $( navigator.push ).trigger({
-                                type: "push",
-                                message: message
-                            });
-                        }
-                    });
-                }
-            }
-        }
-    });
-})( AeroGear, jQuery );
-
 /**
     The AeroGear.Notifier namespace provides a messaging API. Through the use of adapters, this library provides common methods like connect, disconnect, subscribe, unsubscribe and publish.
     @class
@@ -662,6 +570,98 @@ function bindSubscribeSuccess( channelID, request ) {
         request.onsuccess( event );
     });
 }
+
+(function( AeroGear, $, undefined ) {
+    var simpleNotifier, nativePush;
+    // Use browser push implementation when available
+    // TODO: Test for browser-prefixed implementations
+    // TODO: Actually use the native version when present
+    if ( navigator.push ) {
+        nativePush = navigator.push;
+    }
+
+    // SimplePush Default Config
+    AeroGear.SimplePush = window.AeroGearSimplePush;
+    AeroGear.SimplePush.simplePushServerURL = window.AeroGearSimplePush.simplePushServerURL || "http://" + window.location.hostname + ":7777/simplepush";
+
+    // Add push to the navigator object
+    navigator.push = (function() {
+        return {
+            register: nativePush ? nativePush.register : function() {
+                var request = {
+                    onsuccess: function( event ) {}
+                };
+
+                if ( !simpleNotifier ) {
+                    throw "SimplePushConnectionError";
+                }
+
+                simpleNotifier.subscribe({
+                    requestObject: request,
+                    callback: function( message ) {
+                        $( navigator.push ).trigger({
+                            type: "push",
+                            message: message
+                        });
+                    }
+                });
+
+                return request;
+            },
+
+            unregister: nativePush ? nativePush.unregister : function( endpoint ) {
+                simpleNotifier.unsubscribe( endpoint );
+            }
+        };
+    })();
+
+    navigator.setMessageHandler = function( messageType, callback ) {
+        var handler;
+        // TODO: Check for other browser implementations
+        if ( navigator.mozSetMessageHandler ) {
+            navigator.mozSetMessageHandler.apply( arguments );
+            return;
+        }
+
+        handler = function( event ) {
+            var message = event.message;
+            callback.call( this, message );
+        };
+
+        $( navigator.push ).on( messageType, handler );
+    };
+
+    // Create a Notifier connection to the Push Network
+    simpleNotifier = AeroGear.Notifier({
+        name: "agPushNetwork",
+        type: "SimplePush",
+        settings: {
+            connectURL: AeroGear.SimplePush.simplePushServerURL
+        }
+    }).clients.agPushNetwork;
+
+    simpleNotifier.connect({
+        onConnect: function( data ) {
+            var pushStore = JSON.parse( localStorage.getItem("ag-push-store") || '{}' ),
+                channels = pushStore.channels || [];
+
+            // Subscribe to any new channels
+            for ( var channel in channels ) {
+                if ( channels[ channel ].state === "new" ) {
+                    simpleNotifier.subscribe({
+                        channelID: channels[ channel ].channelID,
+                        callback: function( message ) {
+                            $( navigator.push ).trigger({
+                                type: "push",
+                                message: message
+                            });
+                        }
+                    });
+                }
+            }
+        }
+    });
+})( AeroGear, jQuery );
 
 (function( AeroGear, $, undefined ) {
     /**
